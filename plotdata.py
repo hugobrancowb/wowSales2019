fca975865d4f2c38bf0726a1459da7d679705b8b
Hugo <hugobrancowb@gmail.com>
2020-01-21 12:55:31 -0300

    new function which_day to convert string to datetime (y, m, d)

--- plotdata.py
+++ plotdata.py
@@ -1,25 +1,22 @@
 from datetime import datetime, date, timedelta
-import numpy as np
+# import numpy as np
 
 def plot(allSales):
-    create_list_of_days(allSales)
+    calendar = create_list_of_days(allSales)
 
 def create_list_of_days(allSales):
     calendar_full = [datetime.today()] * len(allSales)
     print("")
     for i,entry in enumerate(allSales):
-        t = entry.time
-        t = t.split("-")
-        for j in range(0,3):
-            t[j] = int(t[j])
-        calendar_full[i] = date(t[0], t[1], t[2])
+        calendar_full[i] = which_day(entry.time)
 
     calendar_full.sort()
     calendar = fill_list_of_days(calendar_full)
 
-    # calendar = np.unique(calendar_full)
     for calendar_day in calendar:
         print("{}".format(calendar_day.isoformat()))
+    
+    return calendar
 
 def fill_list_of_days(calendar_full):
     start = calendar_full[0]
@@ -32,3 +29,9 @@ def fill_list_of_days(calendar_full):
         dia = dia + timedelta(days=1)
     
     return calendar
+
+def which_day(string):
+    t = string.split("-")
+    for j in range(0,3):
+            t[j] = int(t[j])
+    return date(t[0], t[1], t[2])
