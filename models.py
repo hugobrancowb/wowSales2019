acce8924ae6450baf2a94820afa4273449e4e3b7
Hugo <hugobrancowb@gmail.com>
2020-01-22 23:20:12 -0300

    indentation fix

--- models.py
+++ models.py
@@ -1,24 +1,24 @@
 class Transaction:
     def __init__(self, itemName, stackSize, quantity, price, otherPlayer, player, time, source):
-        self.itemName    = itemName
-        self.stackSize   = stackSize
-        self.quantity    = quantity
-        self.price       = price
+        self.itemName = itemName
+        self.stackSize = stackSize
+        self.quantity = quantity
+        self.price = price
         self.otherPlayer = otherPlayer
-        self.player      = player
-        self.time        = time
-        self.source      = source
+        self.player = player
+        self.time = time
+        self.source = source
 
     def serialize(self):
         return {
-            "itemName":      self.itemName,
-            "stackSize":     self.stackSize,
-            "quantity":      self.quantity,
-            "price":         self.price,
-            "otherPlayer":   self.otherPlayer,
-            "player":        self.player,
-            "time":          str(self.time),
-            "source":        self.source
+            "itemName": self.itemName,
+            "stackSize": self.stackSize,
+            "quantity": self.quantity,
+            "price": self.price,
+            "otherPlayer": self.otherPlayer,
+            "player": self.player,
+            "time": str(self.time),
+            "source": self.source
         }
 
 def transactionFromJSON(json):
